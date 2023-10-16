0e88ab46019cff31b01ef6ee2c10dd91c7a0648c
Facuindo <facu_nicogas@outlook.com>
2023-10-16 07:31:10 -0300

    ejercicio de manejo de archivos

---
+++ pi-23_7C1.py
@@ -0,0 +1,13 @@
+
+import csv
+
+
+def desafio():
+    print('Ejercicio de archivos')    
+
+    archivo = 'stock.csv'
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+   
---
+++ pi-23_7C2.py
@@ -0,0 +1,13 @@
+
+
+import csv
+
+
+def desafio(ambientes):
+    print('Ejercicios con archivos CSV complejos')
+    archivo = 'propiedades.csv'
+    
+    
+if __name__ == '__main__':
+    
+
