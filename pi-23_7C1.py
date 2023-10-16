8fc5eaff512bc89e5dfe3d44523800480e2ea3fa
Facuindo <facu_nicogas@outlook.com>
2023-10-16 07:36:06 -0300

    ejercicio csv stock

--- pi-23_7C1.py
+++ pi-23_7C1.py
@@ -7,6 +7,13 @@ def desafio():
 
     archivo = 'stock.csv'
 
+    with open(archivo) as csvfile:
+        data = list(csv.DictReader(csvfile))
+    total_tornillos = 0
+    for i in range(len(data)):
+        cantidad_tornillos = float(data[i]["tornillos"])
+        total_tornillos += cantidad_tornillos
+    return total_tornillos
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
